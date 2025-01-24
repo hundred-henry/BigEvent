6c2645e3c262cc0d10c54bb60ad2dcc5992d1b3f
hundred-henry <onehundred.hongji@gmail.com>
2025-01-23 23:20:12 -0600

    register validate

--- bigevent-backend/src/main/java/com/bigevent/bigeventbackend/controller/UserController.java
+++ bigevent-backend/src/main/java/com/bigevent/bigeventbackend/controller/UserController.java
@@ -3,6 +3,8 @@ package com.bigevent.bigeventbackend.controller;
 import com.bigevent.bigeventbackend.pojo.Result;
 import com.bigevent.bigeventbackend.pojo.User;
 import com.bigevent.bigeventbackend.service.UserService;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,25 +12,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username, String password) {
-        if (username!=null && username.length()>=5 && username.length()<=16 &&
-                password!=null && password.length()>=5 && password.length()<=16) {
-            // Select User
-            User u = userService.findByUserName(username);
-            if (u == null) {
-                userService.register(username, password);
-                return Result.success();
-            } else {
-                return Result.error("username already exists");
-            }
+    public Result register(@Pattern(regexp = "^\\S{5,16}$")String username, @Pattern(regexp = "^\\S{5,16}$")String password) {
+        // Select User
+        User u = userService.findByUserName(username);
+        if (u == null) {
+            userService.register(username, password);
+            return Result.success();
         } else {
-            return Result.error("Illegal parameters");
+            return Result.error("username already exists");
         }
     }
 }
